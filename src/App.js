6ec87f2d8d59f102d55a32ba9077aabccc7f74f6
Emily Wirtz <ewirtz3@gmail.com>
2021-02-13 14:49:36 -0600

    pulled App component into App.js and started todo list

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid, makeStyles, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>ToDo List</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
